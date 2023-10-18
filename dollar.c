9eefa1851927d498cdd65f491f178f5013746c30
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-18 22:33:18 +0500

    3rd in dollar.c

--- dollar.c
+++ dollar.c
@@ -5,6 +5,13 @@ printf("\nMade by : MURTAZA TANVEER\nReg no : SP23-BSE0-96");
 	
 printf("\n\n___________________WELCOME TO CURRENCY CONVERSION PLATFOAM___________________\n\n\n");
 
+float dollar;
+printf("\nEnter Amount In USD : ");
+scanf("%f",&dollar);
+
+float fee;
+fee =  ( ( 10*d ) / ( 100 ) );
+
 
 
 
