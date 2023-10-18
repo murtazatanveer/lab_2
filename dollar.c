8f757db5e78972d3fe72e28d77c27799e78587e3
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-18 22:31:52 +0500

    2nd in dollar.c

--- dollar.c
+++ dollar.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main(){
 
+printf("\nMade by : MURTAZA TANVEER\nReg no : SP23-BSE0-96");
+	
+printf("\n\n___________________WELCOME TO CURRENCY CONVERSION PLATFOAM___________________\n\n\n");
 
 
 
 
 
 
-    
 }
